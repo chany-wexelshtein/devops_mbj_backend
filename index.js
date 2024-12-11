6859b225b3f1fe3eefd1da7ff7ed4c0c93889be4
chany-wexelshtein <chany5268@gmail.com>
2024-12-11 16:25:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 5000;
 
 
 // Enable CORS for all routes
-app.use(cors());
+app.use(cors({
+    origin: 'http://34.120.129.160:80' 
+}));
 
 app.get('/api', (req, res) => {
     res.send({ message: 'Hello from backend!' });
